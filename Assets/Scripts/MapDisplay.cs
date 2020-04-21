ae906aadaea07049952a30c4a2fe16b52f56d73b
iSuddenly <issuddenly@gmail.com>
2020-04-21 20:54:33 +0900

    First commit

---
+++ Assets/Scripts/MapDisplay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Take the noisemap and turn it into a texture
+/// Apply the texture to a plane
+/// </summary>
+public class MapDisplay : MonoBehaviour
+{
+    public Renderer textureRenderer;
+    public void DrawNoiseMap(float[,] noiseMap)
+    {
+        int width = noiseMap.GetLength(0); //1st dimension
+        int height = noiseMap.GetLength(1); //2nd dimension
+
+        Texture2D texture = new Texture2D(width, height);
+
+        Color[] colorMap = new Color[width * height];
+        for(int y=0; y<height; y++)
+        {
+            for(int x=0; x<width; x++)
+            {
+                colorMap[y * width + x] = Color.Lerp(Color.black, Color.white, noiseMap[x, y]);
+            }
+        }
+        texture.SetPixels(colorMap);
+        texture.Apply();
+
+        textureRenderer.sharedMaterial.mainTexture = texture;
+        textureRenderer.transform.localScale = new Vector3(width, 1, height);
+    }
+}
---
+++ Assets/Scripts/Noise.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Generate Noise Map, return grids of values between 0 and 1
+/// </summary>
+public static class Noise
+{
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale)
+    {
+        float[,] noiseMap = new float[mapWidth, mapHeight];
+
+        if(scale <= 0) scale = 0.0001f;//Avoid division by zero error
+
+        for(int y = 0; y< mapHeight; y++)
+        {
+            for(int x = 0; x < mapWidth; x++)
+            {
+                float sampleX = x / scale; //divide by scale to get non-integer values
+                float sampleY = y / scale;
+
+                float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
+                noiseMap[x, y] = perlinValue;
+            }
+        }
+        return noiseMap;
+    }
+}
